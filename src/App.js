f6660ed593428cce55fef6783e297eca05b8846e
darrien <46081342+darrienchow@users.noreply.github.com>
2022-05-23 22:46:36 -0700

    add home link to point to ecommerce

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
 
                     <Routes>
                         {/* dashboard  */}
-                        <Route path="/" element={<Ecommerce />} />
+                        <Route path="/ecommerce-dashboard" element={<Ecommerce />} />
                         <Route path="/ecommerce" element={<Ecommerce />} />
 
                         {/* pages  */}
